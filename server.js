906977e81c9e3670d04db373ccfb375e93b85be1
PiperMH <piper.1.hayden@gmail.com>
2017-10-23 13:57:50 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+
+//npm packages required
+const express = require('express');
+const bodyParser = requier("body-parser");
+
+//creating an express server
+var app = express();
+
+//setting the inital port
+var PORT = process.env.PORT || 8080;
+
+//package used to help sevrer interperet incoming data
+app.use(bodyParser.json());
+app.use(bodyParser.urlendcoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+//routes/directions for server to specific files
+require("./app/routing/apiRoute")(app);
+require("./app/routing/htmlRoute")(app);
