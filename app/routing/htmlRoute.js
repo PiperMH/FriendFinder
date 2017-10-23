cd936886993c0098e00d258be3e9001f03a744bc
PiperMH <piper.1.hayden@gmail.com>
2017-10-23 14:26:01 -0500

    html route js

---
+++ app/routing/htmlRoute.js
@@ -0,0 +1,18 @@
+
+//npm packege
+var path = require("path");
+
+
+//route to a specific page depending on the visitor's choice
+
+module.exports = function(app){
+
+	app.get("/survey", function(request, response){
+		response.sendFile(path.join(__dirname, "/../public/survey.html"));
+	});
+
+	app.get("*", function(request, response){
+		response.sendFile(path.join(__dirname,"/../public/home.html"));
+	});
+
+}.
